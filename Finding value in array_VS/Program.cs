c82e8fb865b76661fd3f982d8bcdb3e0683b61b1
Doan Minh Duy <duydoan812@gmail.com>
2025-03-17 22:17:07 +0700

    Finish Commit

---
+++ Finding value in array_VS/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] student = { "Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily" };
+Console.WriteLine("Enter student name: ");
+string studentname = Console.ReadLine();
+
+bool isExist = false;
+for (int i = 0; i < student.Length; i++)
+{
+    if (student[i].Equals(studentname))
+    {
+        Console.WriteLine("Position of the students in the list " + student[i] + " is: " + (i + 1));
+        isExist = true;
+        break;
+    }
+}
+if (!isExist)
+{
+    Console.WriteLine("Not Found " + studentname + " in the list.");
+}
